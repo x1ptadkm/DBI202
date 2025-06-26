9a36402c94476446921822a3f700b4b8100f81c0
Anh Pham Tuan <x1ptadkm@gmail.com>
2025-06-26 13:02:34 +0700

    update src

---
+++ FUH_COMPANY/FUH_COMPANY_BT3.sql
@@ -0,0 +1,18 @@
+﻿SELECT * FROM tblEmployee
+WHERE 
+
+--2. THÔNG TIN NHÂN VIÊN KHÔNG THAM GIA DỰ ÁN NÀO CẢ
+SELECT * FROM tblEmployee
+EXCEPT
+SELECT DISTINCT E.*
+FROM tblEmployee E	
+	JOIN tblWorksOn O ON E.empSSN=O.empSSN
+
+SELECT * 
+FROM tblEmployee E
+LEFT JOIN tblWorksOn O ON E.empSSN=O.empSSN
+WHERE O.empSSN IS NULL
+
+SELECT * 
+FROM tblEmployee E
+RIGHT JOIN tblWorksOn O ON E.empSSN=O.empSSN
